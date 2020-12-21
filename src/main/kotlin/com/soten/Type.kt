e4ca1b33de64ccf12097c5289c5458bed96945e2
Choiyounho <maxosa78@gmail.com>
2020-12-21 23:07:40 +0900

    [learn] : 변수 타입 설정

---
+++ src/main/kotlin/com/soten/Type.kt
@@ -0,0 +1,12 @@
+package com.soten
+
+// 변수의 타입 설정 법
+var number1 : Int = 1000000000
+var number2 : Long = 1000000000000000000
+var version : Double = 1.4
+
+fun main() {
+    println(number1)
+    println(number2)
+    println(version.toInt())
+}
