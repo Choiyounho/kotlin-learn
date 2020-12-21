7d99da8c4ee9b0ff2bdd1e7d81644589c334ea51
Choiyounho <maxosa78@gmail.com>
2020-12-21 22:51:23 +0900

    [learn] : 코틀린 변수 이해

---
+++ src/main/kotlin/com/soten/Variable.kt
@@ -0,0 +1,23 @@
+package com.soten
+
+var year = 2020 // 변하는 변수
+var hello = "hello"
+val number = 100 // 변하지 않는 변수 Java 로는 final 변수
+
+
+
+fun main() {
+
+    println(year + 1)
+    println(hello)
+    println(number)
+
+    year = 2011
+    hello = "dczx"
+//  number = 105 Val cannot be reassigned
+
+    println(year)
+    println(hello)
+    println(number)
+
+}
