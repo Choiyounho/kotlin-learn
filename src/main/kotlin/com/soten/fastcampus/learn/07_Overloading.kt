16fc6ed04d5ae2f788629c5dbe457c36ec83cf51
Choiyounho <maxosa78@gmail.com>
2020-12-31 23:43:17 +0900

    [learn] : 오버로딩

---
+++ src/main/kotlin/com/soten/fastcampus/learn/07_Overloading.kt
@@ -0,0 +1,15 @@
+package com.soten.fastcampus.learn
+
+fun test(number1: Int) {
+	println(number1)
+}
+
+fun test(number1: Int, number2: Int) {
+	println(number1)
+	println(number2)
+}
+
+fun main() {
+	test(1)
+	test(2, 3)
+}
