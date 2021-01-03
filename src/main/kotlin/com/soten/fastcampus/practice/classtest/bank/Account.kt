9365f41dd6eb7435168ac2f9566b0321c4097782
Choiyounho <maxosa78@gmail.com>
2021-01-04 00:08:16 +0900

    [refactor] : 계좌 리팩토링

--- src/main/kotlin/com/soten/fastcampus/practice/classtest/bank/Account.kt
+++ src/main/kotlin/com/soten/fastcampus/practice/classtest/bank/Account.kt
@@ -2,24 +2,23 @@ package com.soten.fastcampus.practice.classtest.bank
 
 class Account(val name: String, val birthday: String) {
 
-	private var balance: Int = 0
+	var balance: Int = 0
 
-	fun checkBalance() {
-		println("잔액 : $balance 원")
+	fun checkBalance():String {
+		return "잔액 : $balance 원"
 	}
 
-	fun deposit(money: Int) {
+	fun deposit(money: Int): String {
 		balance += money
-		println("$balance 원이 입금되었습니다.")
-		checkBalance()
+		return "$balance 원이 입금되었습니다."
 	}
 
-	fun withdrawal(money: Int) {
+	fun withdrawal(money: Int): String {
 		if (balance >= money) {
 			balance -= money
-			println("$balance 원이 출금되었습니다.")
+			return "$balance 원이 출금되었습니다."
 		} else {
-			println("잔액이 부족합니다.")
+			throw IllegalArgumentException("잔액이 부족합니다.")
 		}
 	}
 
