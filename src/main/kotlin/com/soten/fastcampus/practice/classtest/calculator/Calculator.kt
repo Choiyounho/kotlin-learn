9257438555de4cfbacc7dd8d7a55445b2431d1a8
Choiyounho <maxosa78@gmail.com>
2021-01-03 23:20:25 +0900

    [refactor] : 계산기 리팩토링

--- src/main/kotlin/com/soten/fastcampus/practice/classtest/calculator/Calculator.kt
+++ src/main/kotlin/com/soten/fastcampus/practice/classtest/calculator/Calculator.kt
@@ -2,29 +2,35 @@ package com.soten.fastcampus.practice.classtest.calculator
 
 class Calculator(var initNumber: Int) {
 
-	fun clear() {
+	fun clear():Int {
 		initNumber = 0
 		println(initNumber)
+		return initNumber
 	}
 
-	fun plus(number: Int) {
+	fun plus(number: Int): Int {
 		initNumber += number
 		println(initNumber)
+		return initNumber
 	}
 
-	fun minus(number: Int) {
+
+	fun minus(number: Int): Int {
 		initNumber -= number
 		println(initNumber)
+		return initNumber
 	}
 
-	fun multiply(number: Int) {
+	fun multiply(number: Int): Int {
 		initNumber *= number
 		println(initNumber)
+		return initNumber
 	}
 
-	fun divide(number: Int) {
+	fun divide(number: Int): Int {
 		initNumber /= number
 		println(initNumber)
+		return initNumber
 	}
 
 }
