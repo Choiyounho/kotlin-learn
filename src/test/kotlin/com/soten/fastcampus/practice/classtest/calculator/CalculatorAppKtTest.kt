2d37ac6a53af1ab5dd1d024367182d565409f79d
Choiyounho <maxosa78@gmail.com>
2021-01-03 23:37:57 +0900

    [refactor] : 계산기 테스트코드 리팩토링

--- src/test/kotlin/com/soten/fastcampus/practice/classtest/calculator/CalculatorAppKtTest.kt
+++ src/test/kotlin/com/soten/fastcampus/practice/classtest/calculator/CalculatorAppKtTest.kt
@@ -6,43 +6,35 @@ import org.junit.jupiter.api.Test
 
 internal class CalculatorAppKtTest {
 
+	private val cal: Calculator = Calculator(5)
+
 	@Test
 	@DisplayName("계산기 초기화")
 	fun clear() {
-		val cal = Calculator(5)
-
 		assertThat(cal.clear()).isEqualTo(0)
 	}
 
 	@Test
 	@DisplayName("더하기 테스트")
 	fun plus() {
-		val cal = Calculator(5)
-
 		assertThat(cal.plus(5)).isEqualTo(10)
 	}
 
 	@Test
 	@DisplayName("빼기 테스트")
 	fun minus() {
-		val cal = Calculator(5)
-
 		assertThat(cal.minus(5)).isEqualTo(0)
 	}
 
 	@Test
 	@DisplayName("곱하기 테스트")
 	fun multiply() {
-		val cal = Calculator(5)
-
 		assertThat(cal.multiply(2)).isEqualTo(10)
 	}
 
 	@Test
 	fun divide() {
-		val cal = Calculator(10)
-
-		assertThat(cal.divide(2)).isEqualTo(5)
+		assertThat(cal.divide(5)).isEqualTo(1)
 	}
 
 }
