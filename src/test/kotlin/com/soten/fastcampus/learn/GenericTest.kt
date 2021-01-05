ceb9781b3149961bae5366978cde57914e28b9bb
Choiyounho <maxosa78@gmail.com>
2021-01-05 17:04:32 +0900

    [learn] : 제네릭

--- src/test/kotlin/com/soten/fastcampus/learn/GenericTest.kt
+++ src/test/kotlin/com/soten/fastcampus/learn/GenericTest.kt
@@ -1,4 +1,24 @@
 package com.soten.fastcampus.learn
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class GenericTest {
+
+	val list = listOf(1, 2, 3, "가")
+
+	@Test
+	@DisplayName("제너릭 미사용")
+	fun unusedGeneric() {
+		assertThat(list[2].toString()).isEqualTo("3")
+	}
+
+	@Test
+	@DisplayName("제네릭 사용")
+	fun useGeneric() {
+		val list2 = listOf<String>("a", "b", "c")
+		assertThat(list2[2]).isEqualTo("c")
+	}
+
 }
